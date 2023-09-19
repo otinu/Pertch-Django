8997d4b0b21f3ee79730a83b05ff2266f0c8cca2
otinu <fitshter@gmail.com>
2023-09-20 04:07:28 +0900

    pet/indexの画面表示を修正しました

--- petApp/models.py
+++ petApp/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.core.validators import MinLengthValidator
+
+
+class PetModel(models.Model):
+    # (name)最小文字数2,最大文字数20
+    name = models.CharField(validators=[MinLengthValidator(2)], max_length=20)
+    age = models.CharField(validators=[MinLengthValidator(0)], max_length=99)
+    sex = models.BooleanField()
+    charmPoint = models.CharField(max_length=1000)
+    # ToDo 住所のバリデーション(正規表現)
+    # ToDo 7文字ちょうどを指定したい
+    postCord = models.CharField(validators=[MinLengthValidator(7)], max_length=8)
+    address = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="pet_image/")
+
+    now = datetime.datetime.now()
+    created_at = models.DateTimeField(default=now)
+    updated_at = models.DateTimeField(default=now)
+
+    # ToDo Owner・PetCommentとのリレーション
--- petApp/views.py
+++ petApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from .models import PetModel
 
 
 def index(request):
-    return render(request, "pet/index.html")
+    list = PetModel.objects.all()
+    return render(request, "pet/index.html", {"list": list})
