dbaa2791f3ced50487c029d537ed009fc3457502
otinu <fitshter@gmail.com>
2023-09-23 15:37:34 +0900

    リクエストパラメータ取得用メソッド(get_value_or_empty)を追加しました

--- petApp/views.py
+++ petApp/views.py
@@ -16,22 +16,18 @@ def index(request):
 
 def new(request):
     if request.method == "POST":
-        name = request.POST["name"]
-        age = request.POST["age"]
-        sex_string = request.POST["sex"]
+        name = get_value_or_empty(request, "name")
+        age = get_value_or_empty(request, "age")
+        sex_string = get_value_or_empty(request, "sex")
         if sex_string:
             sex = True
         else:
             sex = False
-        charmPoint = request.POST["charmPoint"]
-        postCord = request.POST["postCord"]
-        address = request.POST["address"]
+        charmPoint = get_value_or_empty(request, "charmPoint")
+        postCord = get_value_or_empty(request, "postCord")
+        address = get_value_or_empty(request, "address")
 
-        if ("upload_file", "") in request.POST.items():
-            # ペット登録時に画像なし ⇒ 明示的にNoneを埋め込む
-            image = None
-        else:
-            image = request.FILES["upload_file"]
+        image = get_value_or_empty(request, "upload_file")
 
         # created_at,updated_at用
         now = datetime.datetime.now()
@@ -67,10 +63,8 @@ def new(request):
 
 
 def show(request, id):
-    try:
-        pet = PetModel.objects.get(pk=id)
-    except PetModel.DoesNotExist:
-        traceback.format_exc()
+    pet = get_one_pet(id)
+    if pet is None:
         return render(
             request,
             "pet/index.html",
@@ -83,3 +77,80 @@ def show(request, id):
         "pet/show.html",
         context={"pet": pet, "pet_comment_list": pet_comment_list},
     )
+
+
+def edit(request, id):
+    """
+    pet = get_one_pet(id)
+    if pet is None:
+        return render(
+            request,
+            "pet/index.html",
+            {"error_message": "予期せぬエラーが発生しました\n管理者にご確認ください"},
+        )
+
+    if request.method == "POST":
+        name = request.POST["name"]
+        age = request.POST["age"]
+        sex_string = request.POST["sex"]
+        if sex_string:
+            sex = True
+        else:
+            sex = False
+        charmPoint = request.POST["charmPoint"]
+        postCord = request.POST["postCord"]
+        address = request.POST["address"]
+
+        image = get_value_or_empty(request, "upload_file")
+
+        # created_at,updated_at用
+        now = datetime.datetime.now()
+        today = now.strftime("%Y-%m-%d")
+
+        # ログインユーザーとPetの投稿を紐づけ
+        owner = get_current_authenticated_user()
+
+        try:
+            PetModel.objects.create(
+                name=name,
+                age=age,
+                sex=sex,
+                charmPoint=charmPoint,
+                postCord=postCord,
+                address=address,
+                image=image,
+                created_at=today,
+                updated_at=today,
+                owner=owner,
+            )
+            return redirect("/pet/index")
+        except IntegrityError as e:
+            traceback.format_exc()
+
+            # ToDo バリデーションメッセージの確認
+            return render(
+                request,
+                "pet/new.html",
+                {"error_message": "ペット登録に問題が発生しました"},
+            )
+
+    return render(request, "pet/edit.html", context={"pet": pet})
+    """
+
+
+def get_one_pet(id):
+    try:
+        return PetModel.objects.get(pk=id)
+    except PetModel.DoesNotExist:
+        traceback.format_exc()
+        return None
+
+
+def get_value_or_empty(request, name):
+    if (name, "") in request.POST.items():
+        return ""
+
+    if name == "upload_file":
+        return request.FILES[name]
+    else:
+        return request.POST[name]
