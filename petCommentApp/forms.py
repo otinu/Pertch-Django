030efb6eee787fc72a8045e995ccf7d0dc3949c4
otinu <fitshter@gmail.com>
2023-10-04 14:58:28 +0900

    [add] PetComment form.py内の不要な記述を削除しました

--- petCommentApp/forms.py
+++ petCommentApp/forms.py
@@ -3,8 +3,6 @@ from .models import PetCommentModel
 
 
 class PetCommentForm(forms.ModelForm):
-    reuqired_message = "入力が必要です"
-
     event_time = forms.DateTimeField(
         label="時間",
         widget=forms.DateTimeInput(
@@ -19,8 +17,7 @@ class PetCommentForm(forms.ModelForm):
     event_place = forms.CharField(
         label="場所",
         max_length=80,  # max_lengthを超過した入力は受け付けなくなる
-        required=True,
-        error_messages={"required": reuqired_message},
+        # required=True, # fields指定をしていれば、requiredは不要?
         widget=forms.TextInput(attrs={"class": "event-text"}),  # HTML側でのclass属性を追加
     )
 
@@ -28,17 +25,8 @@ class PetCommentForm(forms.ModelForm):
         label="情報",
         widget=forms.Textarea(attrs={"class": "event-textarea"}),
         max_length=200,
-        required=True,
-        error_messages={"required": reuqired_message},
     )
 
     class Meta:
         model = PetCommentModel
         fields = ("event_time", "event_place", "event_information")
-        labels = {"event_time": "日時", "event_place": "場所", "event_information": "情報"}
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["event_time"].required = True
-        self.fields["event_place"].required = True
-        self.fields["event_information"].required = True
