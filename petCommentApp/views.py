3cce5b3047afc862fae7a4f14333967af4f15b28
otinu <fitshter@gmail.com>
2023-10-04 14:33:22 +0900

    PetComment Form機能を追加しました

--- petCommentApp/views.py
+++ petCommentApp/views.py
@@ -2,6 +2,7 @@ import traceback
 from datetime import datetime
 
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from .models import PetModel
 from .forms import PetCommentForm
@@ -11,42 +12,25 @@ def new(request):
     form = PetCommentForm(request.POST)
     pet_id = request.POST.get("pet_id")
 
-    # if not form.is_valid():
-    if True:
-        request.META.HTTP_REFERER
+    if not form.is_valid():
+        messages.error(request, "ご入力の際にエラーが発生しました。管理者にご確認ください")
+        return redirect("/pet/show/" + pet_id)
+
+    try:
+        pet = PetModel.objects.get(pk=pet_id)
+    except PetModel.DoesNotExist as e:
+        print(traceback.format_exc())
+        messages.error(request, "予期せぬエラーが発生しました。管理者にご確認ください")
+        return redirect("/pet/index/")
 
     pet_comment = form.save(commit=False)
 
     now = datetime.now()
     today = now.strftime("%Y-%m-%d")
 
-    try:
-        pet = PetModel.objects.get(pk=pet_id)
-    except PetModel.DoesNotExist:
-        traceback.format_exc()
-        return render(
-            request,
-            "pet/index.html",
-            {"error_message": "予期せぬエラーが発生しました\n管理者にご確認ください"},
-        )
-
     pet_comment.created_at = today
     pet_comment.updated_at = today
     pet_comment.pet = pet
 
-    if not pet_comment.is_valid():
-        return render(
-            request,
-            "pet/new.html",
-            {"error_message": "予期せぬエラーが発生しました\n管理者にご確認ください"},
-        )
-
     pet_comment.save()
     return redirect("/pet/show/" + pet_id)
-
-    # ToDo バリデーションメッセージの確認
-    # return render(
-    #     request,
-    #     "petComent/new.html",
-    #     {"error_message": "目撃情報の登録に問題が発生しました"},
-    # )
