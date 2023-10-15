04b9a7c84050969d84a2aa9b4f1119439c4c1b5f
otinu <fitshter@gmail.com>
2023-10-15 12:24:04 +0900

    owner テストコードをリファクタリングしました

--- ownerApp/tests.py
+++ ownerApp/tests.py
@@ -43,166 +43,176 @@ class RegistrationTest(TestCase):
 
     # 通常登録
     def test_registration_normal(self):
+        ## 全入力
         data = {
             "username": "tester",
-            "password": "Tttt1",
+            "password": "Tttt0",
             "contact": "test@sample.jp",
-            "mesage": "登録テスト",
+            "message": "登録テスト",
         }
         form = OwnerForm(data)
         self.assertTrue(form.is_valid())
 
-        data = {
-            "username": "tester2",
-            "password": "Tttt2",
-            "contact": "test2@sample.jp",
-            "mesage": "",
-        }
+        ## Message未入力
+        data["message"] = ""
+        form = OwnerForm(data)
+        self.assertTrue(form.is_valid())
+
+        ### 次テストの準備
+        data["message"] = "登録テスト"
+
+        ## Name(2文字)
+        data["username"] = "t" * 2
+        form = OwnerForm(data)
+        self.assertTrue(form.is_valid())
+
+        ## Name(20文字)
+        data["username"] = "t" * 20
+        form = OwnerForm(data)
+        self.assertTrue(form.is_valid())
+
+        ### 次テストの準備
+        data["username"] = "tester"
+
+        ## Password(4文字-半角英数字,大文字,小文字)
+        data["password"] = "Tt" + "0" * 2
+        form = OwnerForm(data)
+        self.assertTrue(form.is_valid())
+
+        ## Password(12文字-半角英数字,大文字,小文字)
+        data["password"] = "Tt" + "0" * 10
         form = OwnerForm(data)
         self.assertTrue(form.is_valid())
 
     # 異常登録(Name)
     def test_registration_name_abnormal(self):
+        ## Name(1文字)
         data = {
             "username": "t",
-            "password": "Tttt3",
-            "contact": "test3@sample.jp",
-            "mesage": "登録テスト",
+            "password": "Tttt0",
+            "contact": "test@sample.jp",
+            "message": "登録テスト",
         }
         form = OwnerForm(data)
         self.assertFalse(form.is_valid())
 
-        data = {
-            "username": "tester444444444444444",
-            "password": "Tttt2",
-            "contact": "test2@sample.jp",
-            "mesage": "",
-        }
+        ## Name(21文字)
+        data["username"] = "t" * 21
+        form = OwnerForm(data)
+        self.assertFalse(form.is_valid())
+
+        ## Name未入力
+        data["username"] = ""
         form = OwnerForm(data)
         self.assertFalse(form.is_valid())
 
     # 異常登録(Password)
     def test_registration_password_abnormal(self):
+        ## Password(3文字)
         data = {
-            "username": "tester5",
-            "password": "Tt5",
-            "contact": "test5@sample.jp",
-            "mesage": "登録テスト",
+            "username": "tester",
+            "password": "Tt0",
+            "contact": "test@sample.jp",
+            "message": "登録テスト",
         }
         form = OwnerForm(data)
+        if form.errors is not None:
+            self.assertTrue("password" in form.errors)
         self.assertFalse(form.is_valid())
 
-        data = {
-            "username": "tester5",
-            "password": "Tt5",
-            "contact": "test5@sample.jp",
-            "mesage": "登録テスト",
-        }
+        ## Password(13文字-半角英数字,大文字,小文字)
+        data["password"] = "Tttt" + "0" * 9
         form = OwnerForm(data)
+        if form.errors is not None:
+            self.assertTrue("password" in form.errors)
         self.assertFalse(form.is_valid())
 
-        data = {
-            "username": "tester5",
-            "password": "Tttt555555555",
-            "contact": "test5@sample.jp",
-            "mesage": "登録テスト",
-        }
+        ## Password(8文字-半角英数字,大文字)
+        data["password"] = "T" * 7 + "0"
         form = OwnerForm(data)
+        if form.errors is not None:
+            self.assertTrue("password" in form.errors)
         self.assertFalse(form.is_valid())
 
-        data = {
-            "username": "tester6",
-            "password": "TTTTTTT6",
-            "contact": "test6@sample.jp",
-            "mesage": "登録テスト",
-        }
+        ## Password(8文字-半角英数字,小文字)
+        data["password"] = "t" * 7 + "0"
         form = OwnerForm(data)
+        if form.errors is not None:
+            self.assertTrue("password" in form.errors)
         self.assertFalse(form.is_valid())
 
-        data = {
-            "username": "tester7",
-            "password": "ttttttt7",
-            "contact": "test7@sample.jp",
-            "mesage": "登録テスト",
-        }
+        ## Password(8文字-大文字,小文字)
+        data["password"] = "T" * 7 + "t"
         form = OwnerForm(data)
+        if form.errors is not None:
+            self.assertTrue("password" in form.errors)
         self.assertFalse(form.is_valid())
 
-        data = {
-            "username": "tester8",
-            "password": "Tttttttt",
-            "contact": "test8@sample.jp",
-            "mesage": "登録テスト",
-        }
+        ## Password(8文字-半角英数字)
+        data["password"] = "9" * 8
         form = OwnerForm(data)
+        if form.errors is not None:
+            self.assertTrue("password" in form.errors)
         self.assertFalse(form.is_valid())
 
-        data = {
-            "username": "tester9",
-            "password": "99999999",
-            "contact": "test9@sample.jp",
-            "mesage": "登録テスト",
-        }
+        ## Password未入力
+        data["password"] = ""
         form = OwnerForm(data)
-        self.assertFalse(form.is_valid())
-
-        data = {
-            "username": "tester10",
-            "password": "",
-            "contact": "test10@sample.jp",
-            "mesage": "登録テスト",
-        }
-        form = OwnerForm(data)
+        if form.errors is not None:
+            self.assertTrue("password" in form.errors)
         self.assertFalse(form.is_valid())
 
     # 異常登録(Email)
     def test_registration_contact_abnormal(self):
+        ## Email(ドットなし)
         data = {
-            "username": "tester11",
-            "password": "Tttt11",
+            "username": "tester",
+            "password": "Tttt0",
             "contact": "test@test",
-            "mesage": "登録テスト",
+            "message": "登録テスト",
         }
         form = OwnerForm(data)
+        if form.errors is not None:
+            self.assertTrue("contact" in form.errors)
         self.assertFalse(form.is_valid())
 
-        data = {
-            "username": "tester12",
-            "password": "Tttt12",
-            "contact": "testttttttttttttttttttt@test.jp",
-            "mesage": "登録テスト",
-        }
+        ## Email(31文字)
+        data["contact"] = "tes" + "t" * 20 + "@test.jp"
         form = OwnerForm(data)
+        if form.errors is not None:
+            self.assertTrue("contact" in form.errors)
         self.assertFalse(form.is_valid())
 
-        data = {
-            "username": "tester13",
-            "password": "Tttt13",
-            "contact": "",
-            "mesage": "登録テスト",
-        }
+        ## Email未入力
+        data["contact"] = ""
         form = OwnerForm(data)
+        if form.errors is not None:
+            self.assertTrue("contact" in form.errors)
         self.assertFalse(form.is_valid())
 
     # 異常登録(Message)
     def test_registration_message_abnormal(self):
+        ## Message(1001文字)
         over_message = "a" * 1001
         data = {
-            "username": "tester14",
-            "password": "Tttt14",
-            "contact": "test14@sample.jp",
+            "username": "tester",
+            "password": "Tttt0",
+            "contact": "test@sample.jp",
             "message": over_message,
         }
         form = OwnerForm(data)
+        if form.errors is not None:
+            self.assertTrue("message" in form.errors)
         self.assertFalse(form.is_valid())
 
     # 異常登録(重複)
     def test_registration_duplicate_abnormal(self):
+        ## 既存ユーザー(username)
         data = {
             "username": "tester",
             "password": "Tttt1",
             "contact": "test@sample.jp",
-            "mesage": "登録テスト",
+            "message": "登録テスト",
         }
         form = OwnerForm(data)
         form.save()
@@ -210,9 +220,11 @@ class RegistrationTest(TestCase):
             "username": "tester",
             "password": "Tttt15",
             "contact": "test15@sample.jp",
-            "mesage": "登録テスト",
+            "message": "登録テスト",
         }
         form = OwnerForm(data)
+        if form.errors is not None:
+            self.assertTrue("username" in form.errors)
         self.assertFalse(form.is_valid())
 
 
