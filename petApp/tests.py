0b3f0b1265775423113e9b10ac00d25fc910e5d9
otinu <fitshter@gmail.com>
2023-10-15 09:40:52 +0900

    pet/show 自動テストを追加しました

--- petApp/tests.py
+++ petApp/tests.py
@@ -100,7 +100,6 @@ class NewTest(TestCase):
         test_pet = PetModel.objects.get(pk=999)
 
         # Formクラスのバリデーションの代わりにsave()にてテスト実施
-        url = reverse("pet:new")
         with open("media/pet/test.png", "rb") as f:
             file = SimpleUploadedFile(f.name, f.read(), content_type="image/png")
         image = {"image": file}
@@ -138,7 +137,7 @@ class NewTest(TestCase):
         pet.address = test_pet.address
         pet.save()
 
-    # 異常登録(年齢)
+    # 異常登録
     def test_registration_abnormal(self):
         ## 名前(1文字)
         self.data["name"] = "t"
@@ -234,14 +233,13 @@ class NewTest(TestCase):
         self.assertFalse(form.is_valid())
 
 
-"""
 class ShowTest(TestCase):
     def setUp(self) -> None:
-        self.user = Owner.objects.create(
+        user = Owner.objects.create(
             username="tester", password="Test1", contact="test@test.co.jp"
         )
 
-        PetModel.objects.create(
+        pet = PetModel.objects.create(
             id=999,
             name="testPet",
             age=99,
@@ -251,8 +249,27 @@ class ShowTest(TestCase):
             address="test",
             created_at=datetime.now(),
             updated_at=datetime.now(),
-            owner=self.user,
+            owner=user,
         )
+
+        test_pet_comment = PetCommentModel.objects.create(
+            pk=999,
+            event_time=datetime.now(),
+            event_place="test_place",
+            event_information="test_information",
+            created_at=datetime.now(),
+            updated_at=datetime.now(),
+            pet=pet,
+        )
+
+        self.data = {
+            "event_time": test_pet_comment.event_time,
+            "event_place": test_pet_comment.event_place,
+            "event_information": test_pet_comment.event_information,
+            "created_at": datetime.now(),
+            "updated_at": datetime.now(),
+            "pet": test_pet_comment.pet,
+        }
 
     # 初回アクセス
     def test_enter(self):
@@ -275,11 +292,120 @@ class ShowTest(TestCase):
         test_pet = PetModel.objects.get(pk=999)
         response = self.client.get("/pet/show/999", follow=True)
 
+        sex = "♂" if test_pet.sex else "♀"
+
+        owner_a_tag = (
+            '<a id="owner-show-link" href="/owner/detail/'
+            + str(test_pet.owner.pk)
+            + '">'
+        )
+        owner_link = owner_a_tag + test_pet.owner.username + "</a>"
+
+        ## 画面(ペット)
         test_case_list = [
             '<h2 style="margin: 0;">' + test_pet.name + "</h2>",
-            '<i class="fa-solid fa-shield-dog',
+            '<img id="pet-image" src="/static/image/no-image.png">',
+            "年齢",
+            "<p>" + str(test_pet.age) + "歳</p>",
+            "性別",
+            "<p>" + sex + "</p>",
+            "オーナー",
+            owner_link,
+            "特徴",
+            "<textarea readonly>" + test_pet.charm_point + "</textarea>",
+            "郵便番号",
+            "<p>" + str(test_pet.post_cord) + "</p>",
+            "住所",
+            "<p>" + test_pet.address + "</p>",
+            "<small>©Otinu</small>"
+            # breakpoint()でpet.imageが入っている状態でも、
+            # response.content内はno-image.pngになってしまう
+            # ⇒写真の自動テストは負荷
         ]
         for test_case in test_case_list:
             with self.subTest(test_case):
                 self.assertContains(response, test_case)
-"""
+
+        ## 画面(目撃情報)
+        test_case_list = [
+            '<h2 id="eye-witness-title">目撃情報</h2>',
+            "日時",
+            '<input type="datetime-local" name="event_time"',
+            "場所",
+            '<input type="text" name="event_place"',
+            "情報",
+            '<textarea name="event_information"',
+            '<input type="submit" id="eyewitness-button" value="投稿"',
+        ]
+        for test_case in test_case_list:
+            with self.subTest(test_case):
+                self.assertContains(response, test_case)
+
+    # 通常登録
+    def test_registration_normal(self):
+        pet_comment_form = PetCommentForm(self.data)
+        self.assertTrue(pet_comment_form.is_valid())
+
+    # 異常登録
+    ## 日時(20231001)
+    def test_registration_abnormal(self):
+        self.data["event_time"] = datetime(2023, 10, 1).strftime("%Y%m%d")
+        form = PetCommentForm(self.data)
+        if form.errors is not None:
+            self.assertTrue("event_time" in form.errors)
+        self.assertFalse(form.is_valid())
+
+        ## 日時(2023年10月1日)
+        self.data["event_time"] = datetime(2023, 10, 1).strftime("%Y年%m月%d日")
+        form = PetCommentForm(self.data)
+        if form.errors is not None:
+            self.assertTrue("event_time" in form.errors)
+        self.assertFalse(form.is_valid())
+
+        ## 日時(2023/15/01)
+        self.data["event_time"] = "2023/15/01"  # datetimeでは作成不可のため、文字列で代用
+        form = PetCommentForm(self.data)
+        if form.errors is not None:
+            self.assertTrue("event_time" in form.errors)
+        self.assertFalse(form.is_valid())
+
+        ## 日時未入力
+        self.data["event_time"] = None
+        form = PetCommentForm(self.data)
+        if form.errors is not None:
+            self.assertTrue("event_time" in form.errors)
+        self.assertFalse(form.is_valid())
+
+        ### 次テストの準備
+        self.data["event_time"] = datetime.now()
+
+        ## 場所(81文字)
+        self.data["event_place"] = "t" * 81
+        form = PetCommentForm(self.data)
+        if form.errors is not None:
+            self.assertTrue("event_place" in form.errors)
+        self.assertFalse(form.is_valid())
+
+        ## 場所未入力
+        self.data["event_place"] = None
+        form = PetCommentForm(self.data)
+        if form.errors is not None:
+            self.assertTrue("event_place" in form.errors)
+        self.assertFalse(form.is_valid())
+
+        ### 次テストの準備
+        self.data["event_place"] = "test_place"
+
+        ## 情報(201文字)
+        self.data["event_information"] = "t" * 201
+        form = PetCommentForm(self.data)
+        if form.errors is not None:
+            self.assertTrue("event_information" in form.errors)
+        self.assertFalse(form.is_valid())
+
+        ## 情報未入力
+        self.data["event_information"] = None
+        form = PetCommentForm(self.data)
+        if form.errors is not None:
+            self.assertTrue("event_information" in form.errors)
+        self.assertFalse(form.is_valid())
